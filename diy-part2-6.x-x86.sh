8a227f939e052fc8920891a2a53fb230760d00fb
hza81007155 <800755@qq.com>
2026-04-24 08:58:53 +0800

    Update diy-part2-6.x-x86.sh

--- diy-part2-6.x-x86.sh
+++ diy-part2-6.x-x86.sh
@@ -22,13 +22,26 @@ CONFIG_PACKAGE_nginx-util=y
 CONFIG_PACKAGE_nginx-mod-luci=y
 CONFIG_PACKAGE_luci-nginx=y
 CONFIG_PACKAGE_default-settings=y" >> .config
-
-# passwall
-rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
-git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
+---------------------------------------------------------------------
+# Passwall插件
+# 1.清理冲突包（必须，解决coreutils-base64等依赖冲突）
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,ssocks,trojan-go,trojan-plus,xray-plugin}
 rm -rf feeds/luci/applications/luci-app-passwall
+
+# 2.克隆Passwall核心包+LuCI
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/passwall-luci
 
+# 3.选中Passwall及依赖
+make menuconfig <<EOF
+CONFIG_PACKAGE_luci-app-passwall=y
+CONFIG_PACKAGE_luci-i18n-passwall-zh-cn=y
+CONFIG_PACKAGE_coreutils-base64=y
+CONFIG_PACKAGE_xray-core=y
+CONFIG_PACKAGE_v2ray-geodata=y
+CONFIG_PACKAGE_sing-box=y
+EOF
+----------------------------------------------------------------------
 # 追加自定义内核配置项
 echo "CONFIG_PSI=y
 CONFIG_KPROBES=y" >> target/linux/x86/64/config-6.6
